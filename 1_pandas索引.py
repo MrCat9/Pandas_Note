b080f0e088a8f5f78efe19deb9548c6ef637e3b9
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-04-23 08:39:25 +0800

    Create 1_pandas索引.py

---
+++ 1_pandas索引.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+
+dates = pd.date_range('20170101', periods=6)
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+print(df)
+print('='*64)
+print(df.at[dates[0], 'A'])  # 第一行第A列
+print('='*64)
+print(df.loc[dates[1]])  # 第二行
+print('='*64)
+print(df.loc[:, ['A', 'B']])  # 每一行的A,B列
+print('='*64)
+print(df.loc[(df.loc[:, 'A'] >= 0) & (df.loc[:, 'B'] >= 0)])  # 每一行中 A,B列值均大于0的 行
+
